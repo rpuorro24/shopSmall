c16846fe87135bf26215a0f2e5f198348b4ec84d
rpuorro24 <rpuorro@ithaca.edu>
2021-12-06 12:37:11 -0500

    updated login and register routes

--- app/routes.py
+++ app/routes.py
@@ -58,13 +58,32 @@ def business(name):
 # def add_review():
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/customer_login', methods=['GET', 'POST'])
+def customer_login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = Customer.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+            return redirect(next_page)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/owner_login', methods=['GET', 'POST'])
+def owner_login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = BusinessOwner.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
